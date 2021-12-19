d33c507882d6e02fe8617cb6dcd06867bb59c3ea
Rory Sedgwick <rory.sedgwick@arup.com>
2021-12-19 19:39:30 +0000

    hardcode public mapbox token

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,6 @@
 import KeplerGl from 'kepler.gl';
 
-// let token = 'pk.eyJ1Ijoicm9yeXNlZGdpd2tjIiwiYSI6ImNra3ZlZno0dTEzdmwydWxtZmIya3YwNmgifQ.WW0oOt922mpoAOeycwEkhw';
-let token = process.env.REACT_APP_MAPBOX_API_TOKEN;
-console.log('process.env: ', process.env);
+let token = 'pk.eyJ1Ijoicm9yeXNlZGdpd2tjIiwiYSI6ImNra3ZlZno0dTEzdmwydWxtZmIya3YwNmgifQ.WW0oOt922mpoAOeycwEkhw';
 
 const Map = props => (
   <KeplerGl

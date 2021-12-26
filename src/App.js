c27914d6837de4dcbb386fb73053b3efc56ce5ee
Rory Sedgwick <rory.sedgwick@arup.com>
2021-12-26 11:22:49 +0000

    update kepler config to render leg colour by distance

--- src/App.js
+++ src/App.js
@@ -17,10 +17,136 @@ class App extends React.Component {
                     },
                     data: processCsvData(jogleActivityData)
                 },
-                option: {
-                    centerMap: true,
-                    readOnly: true,
+                options: {
+                    centerMap: false,
+                    readOnly: false,
                     keepExistingConfig: false
+                },
+                config: {
+                    visState: {
+                        layers: [{
+                            id: "vt1xups",
+                            type: "geojson",
+                            config: {
+                                dataId: "jogle_trip_legs",
+                                label: "JOGLE trip legs",
+                                columns: {
+                                    geojson: "geometry"
+                                },
+                                isVisible: true,
+                                strokeColorField: {
+                                    name: "distance",
+                                    type: "real"
+                                },
+                                strokeColorScale: "quantile",
+                                visConfig: {
+                                    opacity: 0.8,
+                                    strokeOpacity: 0.8,
+                                    thickness: 1,
+                                    strokeColor: null,
+                                    strokeColorRange: {
+                                        name: "Global Warming",
+                                        type: "sequential",
+                                        category: "Uber",
+                                        colors: [
+                                            "#5A1846",
+                                            "#900C3F",
+                                            "#C70039",
+                                            "#E3611C",
+                                            "#F1920E",
+                                            "#FFC300"
+                                        ]
+                                    },
+                                    radius: 10,
+                                    sizeRange: [0,10],
+                                    radiusRange: [0,50],
+                                    heightRange: [0,500],
+                                    elevationScale: 5,
+                                    stroked: true,
+                                    filled: false,
+                                    enable3d: false,
+                                    wireframe: false
+                                },
+                                hidden: false,
+                                textLabel: [
+                                    {
+                                      field: null,
+                                      color: [255,255,255],
+                                      size: 18,
+                                      offset: [0,0],
+                                      anchor: "start",
+                                      alignment: "center"
+                                    }
+                                ]
+                            }
+                        }],
+                        interactionConfig: {
+                            tooltip: {
+                                fieldsToShow: {
+                                    jogle_trip_legs: [
+                                        {
+                                            name: "name",
+                                            format: null
+                                        },
+                                        {
+                                            name: "distance",
+                                            format: null
+                                        },
+                                        {
+                                            name: "moving_time",
+                                            format: null
+                                        },
+                                        {
+                                            name: "calories",
+                                            format: null
+                                        }
+                                    ]
+                                },
+                                compareMode: false,
+                                compareType: "absolute",
+                                enabled: true
+                            },
+                            brush: {
+                                size: 0.5,
+                                enabled: false
+                            },
+                            geocoder: {
+                                enabled: false
+                            },
+                            coordinate: {
+                                enabled: false
+                            }
+                        },
+                        splitMaps: [],
+                        animationConfig: {
+                            currentTime: null,
+                            speed: 1
+                        }
+                    },
+                    mapState: {
+                        bearing: 0,
+                        dragRotate: false,
+                        latitude: 54.360659999999996,
+                        longitude: -4.074945,
+                        pitch: 0,
+                        zoom: 4.75,
+                        isSplit: false
+                    },
+                    mapStyle: {
+                        styleType: "dark",
+                        topLayerGroups: {},
+                        visibleLayerGroups: {
+                            label: true,
+                            road: true,
+                            border: false,
+                            building: true,
+                            water: true,
+                            land: true,
+                            "3d building": false
+                        },
+                        threeDBuildingColor: [],
+                        mapStyles: {}
+                    }
                 }
             })
         );
--- src/stateStore.js
+++ src/stateStore.js
@@ -1,5 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import keplerGlReducer from 'kepler.gl/reducers';
+import keplerGlReducer, {uiStateUpdaters} from 'kepler.gl/reducers';
 import { taskMiddleware } from "react-palm/tasks";
 
 function appReducer() {
@@ -9,10 +9,21 @@ function appReducer() {
 const initialState = {};
 
 const keplerReducer = keplerGlReducer.initialState({
-    uiState: {
-        activeSidePanel: null,
-        currentModal: null
+  uiState: {
+    readOnly: false,
+    activeSidePanel: null,
+    currentModal: null,
+    mapControls: {
+      ...uiStateUpdaters.DEFAULT_MAP_CONTROLS,
+      visibleLayers: {
+        show: true
+      },
+      mapLegend: {
+        show: true,
+        active: true
+      }
     }
+  }
 });
 
 const reducers = combineReducers({
